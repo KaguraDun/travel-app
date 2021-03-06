d7767ddd9b86a87a19f426a54ac57f3212d6aec4
Vasily Kovnev <v.k95@yandex.ru>
2021-03-06 21:44:51 +0300

    reafactor: change Language Switcher element from list to select

--- myapp/src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ myapp/src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -2,17 +2,17 @@ import './LanguageSwitcher.scss';
 
 const LanguageSwitcher: React.FC = () => {
   return (
-    <ul className="languageSwitcher">
-      <li className="languageSwitcher__item">
-        <button type="button">RU</button>
-      </li>
-      <li className="languageSwitcher__item">
-        <button type="button">EN</button>
-      </li>
-      <li className="languageSwitcher__item">
-        <button type="button">JP</button>
-      </li>
-    </ul>
+    <select className="language-switcher">
+      <option className="language-switcher__lang" value="ru">
+        ru
+      </option>
+      <option className="language-switcher__lang" value="en">
+        en
+      </option>
+      <option className="language-switcher__lang" value="jp">
+        jp
+      </option>
+    </select>
   );
 };
 
