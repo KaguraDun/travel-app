7dbe70b32354a8d8bde654b81b52cadc8030e933
Vasily Kovnev <v.k95@yandex.ru>
2021-03-13 00:36:28 +0300

    feat: add slider to attractions

--- myapp/src/components/AttractionCard/AttractionCard.tsx
+++ myapp/src/components/AttractionCard/AttractionCard.tsx
@@ -1,42 +1,38 @@
 import { useEffect, useState } from 'react';
 
+import { AttractionInfo } from '../../models/AttractionInfo.model';
 import { CountryService } from '../../services/http.service';
 
 import './AttractionCard.scss';
 
-function sleeper(ms) {
-  return function (x) {
-    return new Promise((resolve) => setTimeout(() => resolve(x), ms));
-  };
-}
+type AttractionCardProps = {
+  attraction: AttractionInfo;
+};
 
-const AttractionCard = ({ attraction }: any) => {
+const AttractionCard = ({ attraction }: AttractionCardProps) => {
   const [attractionInfo, setAttractionInfo] = useState(null);
 
   useEffect(() => {
     CountryService.fetchAttractionInfo(attraction.xid)
       .then((response) => response.json())
-      .then((info) => {
-        setAttractionInfo(info);
-        console.log(info);
-      })
-      .then(sleeper(500));
+      .then((info) => setAttractionInfo(info));
   }, []);
+
+  const errorText = '...loading';
+  const isInfoReceived = attractionInfo && !attractionInfo.error;
+  const text = isInfoReceived ? attractionInfo.wikipedia_extracts.text : errorText;
+  const imageUrl = isInfoReceived ? attractionInfo.preview.source : '';
+  const kinds = attraction.kinds.replace(/_/g, ' ').replace(/,/g, ', ');
 
   return (
     <div className="attraction">
       <div className="attraction__info">
         <h2>{attraction.name}</h2>
-        <h3>{attraction.kinds.split(',').join(', ')}</h3>
-        <div className="attraction__text">
-          {attractionInfo ? attractionInfo.wikipedia_extracts.text : null}
-        </div>
+        <h3>{kinds}</h3>
+        <div className="attraction__text">{text}</div>
       </div>
 
-      <img
-        className="attraction__image"
-        src={attractionInfo ? attractionInfo.preview.source : null}
-      />
+      <img className="attraction__image" src={imageUrl} />
     </div>
   );
 };
