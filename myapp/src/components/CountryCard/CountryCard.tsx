9298c9742cc729e8391c65d261e933bc49bb0d54
Vasily Kovnev <v.k95@yandex.ru>
2021-03-16 23:05:41 +0300

    fix: country card size

--- myapp/src/components/CountryCard/CountryCard.tsx
+++ myapp/src/components/CountryCard/CountryCard.tsx
@@ -9,27 +9,17 @@ type CountryCardProps = {
 };
 
 const CountryCard = ({ item }: CountryCardProps) => {
-  const { name, nativeName, capital, flag, currencies } = item;
+  const { name, nativeName, capital, flag } = item;
 
   return (
-    <div className="card">
       <Card>
         <Card.Img className="card__img" src={flag} variant="top" />
         <Card.Body>
           <Card.Title>{name}</Card.Title>
           <Card.Subtitle className="mb-2 text-muted">{capital}</Card.Subtitle>
           <Card.Text>Native name: {nativeName}</Card.Text>
-          {/* <Card.Text>
-            {currencies.map((currency) => (
-              <div>
-                <div>Currency name: {currency.name}</div>
-                <div>Currency symbol: {currency.symbol}</div>
-              </div>
-            ))}
-          </Card.Text> */}
         </Card.Body>
       </Card>
-    </div>
   );
 };
 
